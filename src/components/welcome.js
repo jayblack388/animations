12a1128303bbc2e5a6bdfac9b899a0b611f656e3
John Blackwell <john.blackwell@rtslabs.com>
2019-07-24 12:17:27 -0700

    svg's in place, need to add paralax/screen cap

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -3,8 +3,12 @@ import PropTypes from "prop-types"
 import Button from "./button"
 import { StaticQuery, graphql } from "gatsby"
 import Img from "gatsby-image"
-import { COLORS } from "../styles/constants"
 import bg from "../images/backgrounds/top gradient.svg"
+import phoneFrame from "../images/purple_phone_outline.svg"
+import hand from "../images/hand-01.svg"
+import sw1 from "../images/soundwave1.svg"
+import sw2 from "../images/soundwave2.svg"
+import sw3 from "../images/soundwave3.svg"
 
 const Image = () => (
   <StaticQuery
@@ -53,7 +57,7 @@ const Welcome = ({ siteTitle }) => (
       }}
       src={bg}
     />
-    <div style={{ width: "20%", height: "100%", marginTop: 400 }}>
+    <div style={{ width: "20%", height: "100%", marginTop: 400, marginRight: 100 }}>
       <h1 style={{ fontWeight: 500 }}>Create a</h1>
       <h1 style={{ fontWeight: 700 }}>HowTo</h1>
       <p>
@@ -63,8 +67,68 @@ const Welcome = ({ siteTitle }) => (
       <Button>Get started!</Button>
       <Image />
     </div>
-    <div style={{ width: "80%", height: "100%", marginTop: 400 }}>
-      Parallax/Gifs go here
+    <div
+      style={{
+        width: "75%",
+        height: "100%",
+        marginTop: 400,
+        position: "relative",
+      }}
+    >
+      <div
+        style={{
+          position: "absolute",
+          width: "100%",
+          height: "80%",
+        }}
+      >
+        <img
+          style={{
+            position: "absolute",
+            height: "70%",
+            bottom: "4rem",
+            right: "1rem",
+          }}
+          src={sw1}
+        />
+        <img
+          style={{
+            position: "absolute",
+            height: "70%",
+            bottom: "4rem",
+            right: "1rem",
+          }}
+          src={sw2}
+        />
+        <img
+          style={{
+            position: "absolute",
+            height: "70%",
+            bottom: "4rem",
+            right: "1rem",
+          }}
+          src={sw3}
+        />
+        <img
+          style={{
+            position: "absolute",
+            height: "80%",
+            bottom: "-1rem",
+            right: "5rem",
+          }}
+          src={hand}
+        />
+        <img
+          style={{
+            position: "absolute",
+            height: "85%",
+            top: "-1rem",
+            right: "9.5rem",
+          }}
+          src={phoneFrame}
+        />
+        {/* <img style={{ position: "absolute", height: "100%" }} src={phone} /> */}
+      </div>
     </div>
   </div>
 )
